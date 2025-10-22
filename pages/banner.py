eb05e651c54188b8ee403bd50fe64894beb87232
robinnnnnns <sxru2000@outlook.com>
2025-10-22 23:40:07 +1100

    Update banner.py

--- pages/banner.py
+++ pages/banner.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
 st.title("Sketch")
+st.header(" ")
+st.header("Logo")
+with st.container(width='stretch',height="content"):
+    st.image('assets/logo.png')
+st.text("Inspired by the pink robin, a fascinating and iconic Australian bird, the logo incorporates its geometric shapes and vibrant color. It was hand-drawn by me in Procreate to create a friendly impression of the website.")
+st.header("Banner")
 with st.container(width='stretch',height="content"):
     st.image('assets/banner.png')
+st.text("Drawing inspiration from the birds kookaburra and the Australian magpie, I illustrated their everyday scenes in Procreate based on my photographs. The collage-style hand-drawn technique brings out a warm and engaging character. ")
