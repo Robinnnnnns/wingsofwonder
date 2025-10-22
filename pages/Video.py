23a5bbba3d66148038c4e5e5623b7aa9143375dc
robinnnnnns <sxru2000@outlook.com>
2025-10-22 23:33:52 +1100

    Update Video.py

--- pages/Video.py
+++ pages/Video.py
@@ -11,8 +11,8 @@ tab1, tab2 = st.tabs(["1", "2"])
 with tab1:
     st.header("1")
     with st.container(width=800,border=False):
-      st.video("assets/video1.mp4", loop=True,subtitles="assets/sub.vtt")
+      st.video("assets/video1.MP4", loop=True,subtitles="assets/sub.vtt")
 with tab2:
     st.header("2")
     with st.container(width=800,border=False):
-      st.video("assets/video2.mp4", loop=True,subtitles="assets/sub.vtt")
+      st.video("assets/video2.MP4", loop=True,subtitles="assets/sub.vtt")
